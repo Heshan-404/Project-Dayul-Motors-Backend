6381bc90efce9d37c008a74d901e367e128881f5
Heshan-404 <heshantharushka2002@gmail.com>
2024-05-23 23:44:45 +0530

    ..

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to the Dayul Motors Backend</h1>");
+  res.send("<h1>Welcome to the Dayul Motors Backend...</h1>");
 });
 
 const PORT = process.env.PORT || 3000; // You can still use config.PORT here
